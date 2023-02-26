53340f8cceabae2dfd606ab528c40722d863e018
dorinadejolli <121401708+dorinadejolli@users.noreply.github.com>
2023-02-26 22:41:20 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+<style>
+
+
+footer{
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: maroon;
+    height: 400px;
+    width: 100vw;
+    padding-top: 40px;
+    color: #fff;
+}
+
+.footer-content h4{
+    font-size: 2.1rem;
+    font-weight: 500;
+    text-transform: capitalize;
+    line-height: 3rem;
+    text-align: center;
+}
+
+.footer-content p{
+    max-width: 500px;
+    margin: 10px auto;
+    line-height: 28px;
+    font-size: 14px;
+    color: #cacdd2;
+    text-align: center;
+} 
+
+.socials{
+    list-style: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 1rem 0 3rem 0;
+}
+
+.socials li{
+    margin: 0 10px;
+}
+
+.socials a{
+    text-decoration: none;
+    color: #fff;
+    border: 1.1px solid white;
+    padding: 5px;
+    border-radius: 50%;
+}
+
+
+
+
+.footer-bottom{
+    background: white;
+    width: 100vw;
+    height: 30px;
+    padding: 20px;
+    padding-bottom: 40px;
+    text-align: center;
+} 
+
+.footer-bottom p{  
+  color: maroon;
+   float: left;
+   font-size: 14px;
+   word-spacing: 2px;
+   text-transform: capitalize;
+   text-align: center;
+}
+.footer-bottom p a{
+  color:maroon;
+  font-size: 16px;
+  text-decoration: none;
+  text-align: center;
+}
+.footer-bottom span{
+    text-transform: uppercase;
+    opacity: .4;
+    font-weight: 200;
+} 
+
+@media (max-width:500px) {
+.footer-menu ul{
+  display: flex;
+  margin-top: 10px;
+  margin-bottom: 20px;
+} }
+</style>
+
+
+
+
+</head>
+<body>
+    
+
+    
+        <footer>
+     
+     
+         <div class="footer-content">
+           <h4>A&D Transports </h4>
+           <p>Our website offers a very safe and quick transportation service</p>
+     
+     
+           <ul class="socials">
+             <li><a href="https://www.facebook.com/"><img width="35px" height="45px" src="fb.png"></a></li>
+             <li><a href="https://twitter.com/?lang=en"><img width="35px" height="45px"  src="twww.png"></a></li>
+           
+             <li><a href="https://youtube.com"><img width="35px" height="45px"  src="yt.png"></a></li>
+             <li><a href="https://www.linkedin.com"><img width="35px" height="45px"  src="lii.png"></a></li>
+          </ul>
+     
+       </div>
+     
+       <div class="footer-bottom">
+         <p>Copyright &copy;2022 A&D </p>
+      </div>
+     
+     
+     
+     
+     
+     
+     
+        </footer> 
+
+</body>
+</html>
