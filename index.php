adf8d5823fae17e40924f3d2cd95cefcf986b480
dorinadejolli <121401708+dorinadejolli@users.noreply.github.com>
2023-02-26 22:40:47 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>A&D Transports</title> 
+    <link rel="stylesheet" href="stili.css">
+    
+</head>
+<body>
+
+
+<!--  HOMEPAGE  -->
+
+  <div class="banner">
+  <div class="navbar">
+      <svg style="width:35px" 
+      xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" id="IconChangeColor" height="81" width="81"><!--! Font Awesome Free 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2022 Fonticons, Inc. --><path d="M419.2 25.6L496 128H576C593.7 128 608 142.3 608 160V224C625.7 224 640 238.3 640 256C640 273.7 625.7 287.1 608 288C578.8 249.1 532.3 224 480 224C427.7 224 381.2 249.1 351.1 288H288C258.8 249.1 212.3 224 160 224C107.7 224 61.18 249.1 31.99 288C14.32 287.1 0 273.7 0 256C0 238.3 14.33 224 32 224V160C32 142.3 46.33 128 64 128H224V48C224 21.49 245.5 0 272 0H368C388.1 0 407.1 9.484 419.2 25.6H419.2zM288 128H416L368 64H288V128zM168 256C180.1 256 190.1 264.9 191.8 276.6C199.4 278.8 206.7 281.9 213.5 285.6C222.9 278.5 236.3 279.3 244.9 287.8L256.2 299.1C264.7 307.7 265.5 321.1 258.4 330.5C262.1 337.3 265.2 344.6 267.4 352.2C279.1 353.9 288 363.9 288 376V392C288 404.1 279.1 414.1 267.4 415.8C265.2 423.4 262.1 430.7 258.4 437.5C265.5 446.9 264.7 460.3 256.2 468.9L244.9 480.2C236.3 488.7 222.9 489.5 213.5 482.4C206.7 486.1 199.4 489.2 191.8 491.4C190.1 503.1 180.1 512 167.1 512H151.1C139.9 512 129.9 503.1 128.2 491.4C120.6 489.2 113.3 486.1 106.5 482.4C97.09 489.5 83.7 488.7 75.15 480.2L63.83 468.9C55.28 460.3 54.53 446.9 61.58 437.5C57.85 430.7 54.81 423.4 52.57 415.8C40.94 414.1 31.1 404.1 31.1 392V376C31.1 363.9 40.94 353.9 52.57 352.2C54.81 344.6 57.85 337.3 61.58 330.5C54.53 321.1 55.28 307.7 63.83 299.1L75.15 287.8C83.7 279.3 97.09 278.5 106.5 285.6C113.3 281.9 120.6 278.8 128.2 276.6C129.9 264.9 139.9 255.1 151.1 255.1L168 256zM160 432C186.5 432 208 410.5 208 384C208 357.5 186.5 336 160 336C133.5 336 112 357.5 112 384C112 410.5 133.5 432 160 432zM448.2 276.6C449.9 264.9 459.9 256 472 256H488C500.1 256 510.1 264.9 511.8 276.6C519.4 278.8 526.7 281.9 533.5 285.6C542.9 278.5 556.3 279.3 564.9 287.8L576.2 299.1C584.7 307.7 585.5 321.1 578.4 330.5C582.1 337.3 585.2 344.6 587.4 352.2C599.1 353.9 608 363.9 608 376V392C608 404.1 599.1 414.1 587.4 415.8C585.2 423.4 582.1 430.7 578.4 437.5C585.5 446.9 584.7 460.3 576.2 468.9L564.9 480.2C556.3 488.7 542.9 489.5 533.5 482.4C526.7 486.1 519.4 489.2 511.8 491.4C510.1 503.1 500.1 512 488 512H472C459.9 512 449.9 503.1 448.2 491.4C440.6 489.2 433.3 486.1 426.5 482.4C417.1 489.5 403.7 488.7 395.1 480.2L383.8 468.9C375.3 460.3 374.5 446.9 381.6 437.5C377.9 430.7 374.8 423.4 372.6 415.8C360.9 414.1 352 404.1 352 392V376C352 363.9 360.9 353.9 372.6 352.2C374.8 344.6 377.9 337.3 381.6 330.5C374.5 321.1 375.3 307.7 383.8 299.1L395.1 287.8C403.7 279.3 417.1 278.5 426.5 285.6C433.3 281.9 440.6 278.8 448.2 276.6L448.2 276.6zM480 336C453.5 336 432 357.5 432 384C432 410.5 453.5 432 480 432C506.5 432 528 410.5 528 384C528 357.5 506.5 336 480 336z" id="mainIconPathAttribute" fill="white  "></path></svg>
+     
+      <ul>
+          <li><a>HOME</a></li>
+          <li><a href="show.php" target="_blank">SERVICES</a></li>
+          <li><a href="about.php" target="_blank">ABOUT US</a></li>
+          <li><a href="projektiD.php" target="_blank">CONTACT US</a></li>
+      </ul>
+
+  </div>
+  <div class="content">
+      <h1>A&D Transports</h1>
+      <p>We are offering our truckload transportation that directly takes your goods from the origin to the destination, giving
+      you an efficient end-to-end cargo journey..</p>
+  
+      <div>
+          <a href="login.php" target="_blank"><button type="button" class="butoni"><span></span> Log in</button></a>
+      </div>
+
+  </div>
+
+
+  </div>
+
+
+
+
+
+<!--  SERVICES  -->
+    <div class="container">
+
+        <div class="msg">
+        <div id="slider">
+        <div class="msgcol">
+            <h2>Chiller Truck</h2>
+            <p>A refrigerator truck or chiller lorry (also called a Reefer), is a van or truck designed to carry perishable freight at
+            low temperatures. Most long-distance refrigerated transport by truck is done in articulated trucks pulling refrigerated
+            semi-trailersThe first successful mechanically refrigerated trucks were made for the ice cream industry in 1925.
+            African American inventor Frederick McKinley Jones later contributed to the field. There were around 4 million
+            refrigerated road vehicles in use in 2010 worldwide.</p>
+            
+        </div>
+        
+        <div class="msgcol">
+            <h2>Tipper Truck</h2>
+            <p>A dump truck, known also as a dumping truck, dump trailer, dumper trailer, dump lorry or dumper lorry or a dumper for
+            short, is used for transporting materials (such as dirt, gravel, or demolition waste) for construction as well as coal.
+            In the UK, Australia, South Africa and India the term applies to off-road construction plants only and the road vehicle
+            is known as a tip lorry, tipper lorry (UK, India), tipper truck, tip truck, tip trailer or tipper trailer or simply a
+            tipper (Australia, New Zealand, South Africa).</p>
+           
+        </div>
+        
+        <div class="msgcol">
+            <h2>Cane Truck</h2>
+            <p>A truck or lorry is a motor vehicle designed to transport cargo, carry specialized payloads, or perform other
+            utilitarian work. Trucks vary greatly in size, power, and configuration, but the vast majority feature body-on-frame
+            construction, with a cabin that is independent of the payload portion of the vehicle. Smaller varieties may be
+            mechanically similar to some automobiles. Commercial trucks can be very large and powerful and may be configured to be
+            mounted with specialized equipment, such as in the case of refuse trucks, fire trucks, concrete mixers, and suction
+            excavators.</p>
+            
+        </div>
+        
+        <div class="msgcol">
+            <h2>Car Transporter Truck</h2>
+            <p>A car carrier trailer, also known as a car-carrying trailer, car hauler, or auto transport trailer, is a type of trailer
+            or semi-trailer designed to efficiently transport passenger vehicles via truck.
+            Modern car carrier trailers can be open or enclosed. Most commercial trailers have built-in ramps for loading and
+            off-loading cars, as well as power hydraulics to raise and lower ramps for stand-alone accessibility</p>
+            
+        </div>
+        
+        </div>
+        </div>
+         </div>
+            
+        <div class="controller">
+        <div id="lin1"></div>
+        <div id="lin2"></div>
+        <div id="lin3"></div>
+        <div id="lin4"></div>
+        <div id="active"></div> </div>
+
+<!--  ABOUT  -->
+
+        <section class="about">
+            <div class="main" style="width:100%"> </div>
+            
+             <div class="about-text">
+                 <h3>About us</h3>
+                 <p> A single trucking business manages communications with at least three groups: customers (shippers), truckers, and
+                back-office workers. Timely and accurate communications are critical to each aspect of the business, from customer
+                service to inventory management. Our truckload transport can offer you connecting different Maersk services via One Product,
+                giving you visibility from start to end,
+                more flexibility to design solutions for your business,
+                better quality control thanks to fixed partnerships and our own equipment. The shipment solution that you require varies with the specifications of your cargo. The size of your shipment, the
+                fragility of your cargo, required speed to market, or budget can affect this decision. We’re offering Full Truckload
+                (FTL) and Less than Truckload (LTL) shipments. Our truckload transport offering can also give you some added benefits: &nbsp;
+                
+                Custom-fit: We have a variety of equipment from which you can pick the right solution based on your needs &nbsp; /
+                Short-notice: Truckload transport has a relatively shorter time of pre-booking compared to ocean or other inland solutions &nbsp; /
+              
+                Speed: Conventional trucking is the fastest mode of inland transport &nbsp;
+
+      </p>
+     
+                  <a href="https://en.wikipedia.org/wiki/Freight_transport"> <button type="button">Learn more</button> </a>
+             
+            </div>
+     
+          </section>
+         
+<!--  CONTACT  -->
+
+        
+            
+              
+         
+          
+           <div  style="width: 50%;" class="containerr">  
+            <form id="contact" action="" method="post">
+              <h3>Quick Contact</h3>
+              <h4>Contact us today, and get reply with in 24 hours!</h4>
+              <fieldset>
+                <input placeholder="Your name" type="text" tabindex="1" required autofocus>
+              </fieldset>
+              <fieldset>
+                <input placeholder="Your Email Address" type="email" tabindex="2" required>
+              </fieldset>
+              <fieldset>
+                <input placeholder="Your Phone Number" type="tel" tabindex="3" required>
+              </fieldset>
+            
+              <fieldset>
+                <input placeholder="Your Web Site starts with http://" type="url" tabindex="4" required>
+              </fieldset>
+              <fieldset>
+                <textarea placeholder="Type your Message Here...." tabindex="5" required></textarea>
+              </fieldset>
+              <fieldset>
+                <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+              </fieldset>
+            </form>
+           
+            
+          </div>
+
+
+<!--  FOOTER  -->
+
+          <footer>
+     
+     
+            <div class="footer-content">
+              <h4>A&D Transports </h4>
+              <p>Our website offers a very safe and quick transportation service</p>
+        
+        
+              <ul class="socials">
+                <li><a href="https://www.facebook.com/"><img width="35px" height="45px" src="fb.png"></a></li>
+                <li><a href="https://twitter.com/?lang=en"><img width="35px" height="45px"  src="twww.png"></a></li>
+              
+                <li><a href="https://youtube.com"><img width="35px" height="45px"  src="yt.png"></a></li>
+                <li><a href="https://www.linkedin.com"><img width="35px" height="45px"  src="lii.png"></a></li>
+             </ul>
+        
+          </div>
+        
+          <div class="footer-bottom">
+            <p>Copyright &copy;2022 A&D </p>
+         </div>
+        
+        
+        
+        
+        
+        
+        
+           </footer> 
+
+
+<!--SKRIPTA-->
+           <script>
+
+var slider = document.getElementById('slider');
+var active = document.getElementById('active');
+var lin1 = document.getElementById('lin1');
+var lin2 = document.getElementById('lin2');
+var lin3 = document.getElementById('lin3');
+var lin4 = document.getElementById('lin4');
+
+lin1.onclick = function() {
+    slider.style.transform = 'translateX(0)';
+    active.style.top = '0px';
+}
+lin2.onclick = function() {
+    slider.style.transform = 'translateX(-25%)';
+    active.style.top = '80px';
+}
+lin3.onclick = function() {
+    slider.style.transform = 'translateX(-50%)';
+    active.style.top = '160px';
+}
+lin4.onclick = function() {
+    slider.style.transform = 'translateX(-75%)';
+    active.style.top = '240px';
+}
+
+
+           </script>
+
+</body>
+</html>
